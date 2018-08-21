fc8fbab60090e39af748a967073938d8432238be
wscjxky <619677632@qq.com>
2018-08-21 11:42:08 +0800

    init

---
+++ main_scatter.py
@@ -0,0 +1,26 @@
+import pyecharts
+from pyecharts import Scatter
+
+import numpy as np
+
+model_name = "deepfool"
+data = np.load(model_name + '_grad_data_all.npy')
+dirname = 'output'
+bar_name = "图"
+desc_name = ""
+desc_coor_y = "均值"
+for index in range(data.shape[0], 100):
+    for x_index in range(data.shape[2]):
+        with open('convname.txt', 'r') as f:
+            desc_name = f.readlines()[index]
+        coor_x = data[index][0][x_index]
+        coor_y = data[index][0][x_index]
+        # v1 = [2.0, 4.9, 7.0, 23.2, 25.6, 76.7, 135.6, 162.2, 32.6, 20.0, 6.4, 3.3]
+        # v2 = [2.6, 5.9, 9.0, 26.4, 28.7, 70.7, 175.6, 182.2, 48.7, 18.8, 6.0, 2.3]
+        bar = Scatter(bar_name, desc_name)
+        bar.add(desc_coor_y, coor_x, coor_y, mark_line=["average"], mark_point=["max", "min"])
+        # bar.add("evaporation", attr, v2, mark_line=["average"], mark_point=["max", "min"])
+        bar.render(path=dirname + '/' + str(index) + '.html')
+        with open(model_name + '_results.html', 'a', encoding='utf8') as f:
+            with open(dirname + '/' + str(index) + '.html', 'r', encoding='utf8') as f_r:
+                f.write(f_r.read())
