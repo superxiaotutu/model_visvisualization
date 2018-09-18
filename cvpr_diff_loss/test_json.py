310cc39fef2e62fc8db4378fa727bffab0c30baa
wscjxky <619677632@qq.com>
2018-09-18 09:35:17 +0800

    init

--- cvpr_diff_loss/test_json.py
+++ cvpr_diff_loss/test_json.py
@@ -1,15 +1,14 @@
-import PIL
-from matplotlib.backends.backend_agg import FigureCanvasAgg
+import random
+from PIL import Image
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Polygon
-from mxnet import image
 import matplotlib.pyplot as plt
 import numpy as np
 from pycocotools.coco import COCO
 
 
 def load_abc_type_img(path, box_l):
-    I = PIL.Image.open(path)
+    I = Image.open(path)
     A = I.resize((299, 299)).crop((0, 0, 299, 299))
     A = (np.asarray(A) / 255.0).astype(np.float32)
     mengban = np.ones((299, 299, 3))
@@ -20,7 +19,7 @@ def load_abc_type_img(path, box_l):
         mengban[int(bx[1] * width_change): int((bx[1] + bx[3]) * width_change),
         int(bx[0] * length_change):int((bx[0] + bx[2]) * length_change), :] = 0
         size_of_mengban += np.abs((int(bx[1] * width_change) - int((bx[1] + bx[3]) * width_change)) * (
-        int(bx[0] * length_change) - int((bx[0] + bx[2]) * length_change)))
+            int(bx[0] * length_change) - int((bx[0] + bx[2]) * length_change)))
     B = A * np.abs(mengban - 1)
     C = A * mengban
 
@@ -33,67 +32,114 @@ def load_abc_type_img(path, box_l):
     B = B.astype(np.uint8)
     C = C.astype(np.uint8)
 
-    # plt.imshow(A)
-    # plt.show()
-    # plt.imshow(mengban)
-    # plt.show()
-    # plt.imshow(B)
-    # plt.show()
-    # plt.imshow(C)
-    # plt.show()
     return A.tobytes(), B.tobytes(), C.tobytes(), size_of_mengban / (299 * 299)
 
 
-image_file = "J:/9.15/val2017"
-ana_dir = "J:/9.15/annotations_trainval2017/annotations/"
-coco = COCO(ana_dir + "instances_val2017.json")
-# print(coco.getAnnIds(285))
-# ann=coco.loadAnns(587562)
-with open("J:/9.15/val2017/000000000285.jpg", 'rb') as f:
-    I = image.imdecode(f.read()).asnumpy()
+def save_my_fig(imgname, imgsize):
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+    ax.axis('off')
+    dpi = 400
+    w = imgsize[0] / dpi
+    h = imgsize[1] / dpi
+    plt.gca().xaxis.set_major_locator(plt.NullLocator())
+    plt.gca().yaxis.set_major_locator(plt.NullLocator())
+    plt.gcf().set_size_inches(w, h)
+    plt.margins(0, 0)
+    plt.subplots_adjust(top=1, bottom=0, left=0, right=1, hspace=0, wspace=0)
+    plt.savefig(imgname, format='jpg', transparent=True, dpi=dpi)
+    plt.close()
 
-plt.imshow(I)
-# plt.show()
 
-# annIds=coco.getAnnIds(285)
-# anns = coco.loadAnns(587562)
-anns = [{'segmentation': [
-    [37.31, 373.02, 57.4, 216.61, 67.44, 159.21, 77.49, 113.29, 91.84, 86.03, 123.41, 84.59, 162.15, 96.07, 215.25,
-     86.03, 261.17, 70.24, 285.56, 68.81, 337.22, 68.81, 411.84, 93.2, 454.89, 107.55, 496.5, 255.35, 513.72, 262.53,
-     552.47, 292.66, 586.0, 324.23, 586.0, 381.63, 586.0, 449.08, 586.0, 453.38, 578.3, 616.97, 518.03, 621.27, 444.84,
-     624.14, 340.09, 625.58, 136.32, 625.58, 1.43, 632.75, 7.17, 555.26, 5.74, 414.64]], 'area': 275709.8110500001,
-         'iscrowd': 0, 'image_id': 285, 'bbox': [1.43, 68.81, 584.57, 563.94], 'category_id': 23, 'id': 587562}]
 def paint_ann(ann):
     ax = plt.gca()
     ax.set_autoscale_on(False)
+    plt.axis('off')
     polygons = []
-    color = []
     for seg in ann['segmentation']:
         poly = np.array(seg).reshape((int(len(seg) / 2), 2))
         polygons.append(Polygon(poly))
-    p = PatchCollection(polygons, facecolor=[0, 0, 0], linewidths=0.5, alpha=1)
+    p = PatchCollection(polygons, edgecolors=[0, 0, 0], facecolor=[0, 0, 0], linewidths=5, alpha=1)
     ax.add_collection(p)
-    file_name="a.png"
-    plt.savefig(file_name)
-    return file_name
-def get_front_mask(path,box_list):
-    I = PIL.Image.open(path)
-    A = I.resize((299, 299)).crop((0, 0, 299, 299))
-    A = (np.asarray(A) / 255.0).astype(np.float32)
-    mengban = np.ones((299, 299, 3))
-    width_change, length_change = 299 / np.asarray(I).shape[0], 299 / np.asarray(I).shape[1]
-    size_of_mengban = 0
-    for bx in box_list:
-        mengban[int(bx[1] * width_change): int((bx[1] + bx[3]) * width_change),
-        int(bx[0] * length_change):int((bx[0] + bx[2]) * length_change), :] = 0
 
-        size_of_mengban += np.abs((int(bx[1] * width_change) - int((bx[1] + bx[3]) * width_change)) * (
-            int(bx[0] * length_change) - int((bx[0] + bx[2]) * length_change)))
-        # plt.imshow(A)
-        # plt.show()
-    # B = A * np.abs(mengban - 1)
-    # C = A * mengban
-load_abc_type_img(paint_ann(anns[0]),anns[0])
+
+def get_new_img(front_path, back_path, anns):
+    new_img = []
+    mask_front_file = "mask_front.jpg"
+    mask_front_all1_file = "mask_front_all1.jpg"
+    mask_back_file = "mask_back.jpg"
+    new_img_file = "new_img.jpg"
+
+    for index, ann in enumerate(anns):
+        front_img = Image.open(front_path)
+        back_img = Image.open(back_path)
+        front_img_size = front_img.size
+        back_img_size = front_img_size
+
+        back_img = back_img.resize((front_img_size[0], front_img_size[1])).crop(
+            (0, 0, front_img_size[0], front_img_size[1]))
+
+        plt.imshow(np.asarray(front_img))
+        paint_ann(ann)
+        save_my_fig(mask_front_file, front_img_size)
+
+        plt.imshow(np.asarray(back_img))
+
+        paint_ann(ann)
+        save_my_fig(mask_back_file, back_img_size)
+
+        mask_front_all1 = np.ones((front_img_size[1], front_img_size[0], 3))
+        plt.imshow(mask_front_all1)
+        paint_ann(ann)
+        save_my_fig(mask_front_all1_file, front_img_size)
+        mask_front_all1 = (np.asarray(Image.open(mask_front_all1_file)))
+        mask_front_all1 = np.where(mask_front_all1 == 255, [1, 1, 1], mask_front_all1)
+        mask_front_all1 = np.abs(mask_front_all1 - 1)
+        mask_front_img = (np.asarray(Image.open(front_path)))
+        new_front_img = mask_front_img * mask_front_all1
+
+        new_back_img = Image.open(mask_back_file)
+        new_back_img = new_back_img.resize((front_img_size[0], front_img_size[1]))
+        new_back_img = np.asarray(new_back_img)
+
+        new_img = new_front_img + new_back_img
+        plt.imshow(new_img)
+        save_my_fig(new_img_file, front_img_size)
+        new_img = Image.open(new_img_file)
+
+        plt.imshow(new_front_img)
+        plt.show()
+        plt.imshow(new_back_img)
+        plt.show()
+        plt.imshow(new_img)
+        plt.show()
+        plt.close()
+    return new_img
+
+
+dataDir = "J:/9.15/annotations_trainval2017/"
+dataType = 'val2017'
+annFile = '{}/annotations/instances_{}.json'.format(dataDir, dataType)
+img_path = "J:/9.15/val2017/"
+coco = COCO(annFile)
+
+# imgcatIds = coco.getCatIds()[0]
+# imgids = coco.getImgIds()
+# imgid = coco.getImgIds(catIds=imgcatIds)[0]
+# imgannids = coco.getAnnIds(imgid, imgcatIds)
+# anns=coco.loadAnns(imgannids)
+
+# front_path = img_path + coco.loadImgs(imgid)[0]['file_name']
+# back_path = img_path + coco.loadImgs(imgids[random.randint(0,2)])[0]['file_name']
+
+imgid = "000000000724.jpg"
+imgid1 = "000000001296.jpg"
+front_path = img_path + imgid
+back_path = img_path + imgid1
+annids = coco.getAnnIds(724)
+anns = coco.loadAnns(annids)
+print(anns)
+get_new_img(front_path, back_path, anns)
 
 
 
