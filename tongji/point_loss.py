135c0f916d314b9b3f66f68d5a584cf43fa45f77
wscjxky <619677632@qq.com>
2018-09-30 12:09:53 +0800

    init

--- tongji/point_loss.py
+++ tongji/point_loss.py
@@ -1,29 +1,30 @@
 import pandas
 import matplotlib.pyplot as plt
+
 data = pandas.read_csv('out.csv', header=None, sep=',')
 count = int(data.count()[0])
 x_arr = []
 y_arr = []
 x_arr1 = []
 y_arr1 = []
-count_y=0
-count_x=0
-plt.figure(111,(1000,1000))
-for i in range(100):
+count_y = 0
+count_x = 0
+plt.figure(111, (1000, 1000))
+for i in range(10000):
     record = data.iloc[[i]]
     true = record[1]
     ori = record[2]
     adv = record[3]
     yes = record[5].iloc[0]
     if str(yes) == 'Yes':
-        count_y+=1
-        x_arr.append(float(true - ori))
-        y_arr.append(float(ori - adv))
+        count_y += 1
+        x_arr.append(float((true - ori) /( 1 - ori)))
+        y_arr.append(float((ori - adv) / ori))
     else:
-        count_x+=1
-        x_arr1.append(float(true - ori))
-        y_arr1.append(float(ori - adv))
-print(count_y,count_x)
-plt.scatter(x_arr1,y_arr1,color='r',alpha=0.4)
-plt.scatter(x_arr,y_arr,color='b',alpha=0.4)
+        count_x += 1
+        x_arr1.append(float((true - ori) /( 1 - ori)))
+        y_arr1.append(float((ori - adv) / ori))
+print(count_y, count_x)
+plt.scatter(x_arr1, y_arr1, color='r', alpha=0.2)
+plt.scatter(x_arr, y_arr, color='b', alpha=0.2)
 plt.show()
