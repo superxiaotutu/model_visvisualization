198822596f27753262addeb2df092c5c3fdd171c
619677632@qq.com <619677632@qq.com>
2018-10-12 18:53:15 +0800

    ``

--- gan_cam/a.py
+++ gan_cam/a.py
@@ -1,17 +1,21 @@
+fp = open('ILSVRC2012_val_00000293.xml')
+from  matplotlib import  pyplot as plt
 import numpy as np
+def get_gound_truth():
+    ground_truth=np.zeros((299,299,1))
+    for p in fp:
+        if '<size>' in p :
+            width=int(next(fp).split('>')[1].split('<')[0])
+            height=int(next(fp).split('>')[1].split('<')[0])
 
-a=np.asarray(
-    [[0,0,0,0,0,0,0,0],
-     [1,1,1,1,1,0,0,1]]
-)
-b=np.asarray(
-    [[0,0,0,0,0,0,0,0],
-     [1,1,1,1,1,0,0,1]]
-)
-count=0
-c=a+b
+        if '<object>' in p:
+            print(next(fp).split('>')[1].split('<')[0])
+        if '<bndbox>' in p:
+            xmin=int(next(fp).split('>')[1].split('<')[0])
+            ymin=int(next(fp).split('>')[1].split('<')[0])
+            xmax=int(next(fp).split('>')[1].split('<')[0])
+            ymax=int(next(fp).split('>')[1].split('<')[0])
+            return int(xmin/width*299),int(ymin/height*299),int(xmax/height*299),int(ymax/height*299)
+print(get_gound_truth())
 
-
-print(c[c!=0].size)
-iou=c[c == 2].size/c[c!=0].size
-print(iou)
+plt.imshow()
